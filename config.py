ce9acf04dfda691ff74f1122ecf98a657af89452
herepete <herepete@hotmail.com>
2024-11-19 14:14:20 +0000

    update config

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ funds_to_check=10 #used in testing
 max_ongoing_charge=1 #if above this fund gets rejected
 annualized_return_value_check=3 #anything below this will get rejected, used in looking at 1,3 and 5 year values
 min_yield=4 # anything below this gets dropped
-
+write_out_html=1
+write_out_db=1
