ac74c8426dc28868a1653abb273050e7c9320442
herepete <herepete@hotmail.com>
2024-11-20 12:06:37 +0000

    tweaks to config

--- inital_setup/os_setup.sh
+++ inital_setup/os_setup.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 yum update
-yum install git
-git clone https://github.com/herepete/Fidelity.git
+#yum install git
+#git clone https://github.com/herepete/Fidelity.git
 yum install pip
 pip3.9 install bs4 pandas
 pip3.9 install openai==0.28
 pip3.9 install flask-cors
+cp flaskapp.service /etc/systemd/system/flaskapp.service
+
 echo "i have installed what i can..."
 echo "Next steps for you"
 echo "To /root/.bashrc add..."
@@ -14,5 +16,8 @@ echo "    -export DB_HOST='localhost'"
 echo "    -export DB_USER='bla'"
 echo "    -export DB_PASSWORD='bla'"
 echo "Then run the install_flask.py & install_mariadb.sh scripts "
-echo "Then run main.py and check the value of the pre-checks & fix anything outstanding"
+echo "Restart appache"
+
+echo "Then run main.py \n\t check the value of the pre-checks & fix anything outstanding\n\tYou will need to start Flask"
+
 
