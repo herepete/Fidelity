d9d60f115e662693c7e06dd5818474ef668e443c
herepete <herepete@hotmail.com>
2024-11-20 11:44:09 +0000

    tweaks to setup

--- inital_setup/install_flask.py
+++ inital_setup/install_flask.py
@@ -1,3 +1,3 @@
-#!/usr/bin/python3
+#!/bin/bash
 pip install flask mysql-connector-python
 
--- inital_setup/os_setup.sh
+++ inital_setup/os_setup.sh
@@ -2,4 +2,17 @@
 yum update
 yum install git
 git clone https://github.com/herepete/Fidelity.git
+yum install pip
+pip3.9 install bs4 pandas
+pip3.9 install openai==0.28
+pip3.9 install flask-cors
+echo "i have installed what i can..."
+echo "Next steps for you"
+echo "To /root/.bashrc add..."
+echo "    -export OPENAI_API_KEY="bla""
+echo "    -export DB_HOST='localhost'"
+echo "    -export DB_USER='bla'"
+echo "    -export DB_PASSWORD='bla'"
+echo "Then run the install_flask.py & install_mariadb.sh scripts "
+echo "Then run main.py and check the value of the pre-checks & fix anything outstanding"
 
